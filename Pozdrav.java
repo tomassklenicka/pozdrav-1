88ae2c3af73dd0cc0894da2d68f9080c9bd7f9c2
ZahorskyOndrej <56340108+ZahorskyOndrej@users.noreply.github.com>
2020-09-07 08:54:40 +0200

    Add files via upload

---
+++ Pozdrav.java
@@ -0,0 +1,11 @@
+
+package javaapplication1;
+
+public class Pozdrav {
+
+    public static void main(String[] args) {
+        System.out.println("Čusan");
+        System.out.println("Záhy");
+    }
+    
+}
