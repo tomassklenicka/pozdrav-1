e6d7da0abcf7e57324a57e8e52f1c201857846f1
tomassklenicka <56340124+tomassklenicka@users.noreply.github.com>
2020-09-07 09:14:10 +0200

    Update Pozdrav.java

--- Pozdrav.java
+++ Pozdrav.java
@@ -6,6 +6,8 @@ public class Pozdrav {
     public static void main(String[] args) {
         System.out.println("Čusan");
         System.out.println("Záhy");
+        System.out.println("bubu");
+        
     }
     
 }
